ae4d1a57961d4f1957778be29592f1510362832e
Igor Dmitriev <idmitrievnz@gmail.com>
2024-02-15 11:41:52 +1300

    displayed data from the internet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const baseUrl = "https://reqres.in/api/users/1"
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    axios.get(baseUrl).then((server_response) => {
+      console.log(server_response.data.data.avatar);
+      this.setState({ picture: server_response.data.data.avatar })
+    })
+
+    this.state = {
+      picture: ""
+    }
+    //All custom functions binds will be here
+  }
+
+  render() {
+    return (
+
+      <div>
+        <header><h1>HEADER</h1></header>
+        <img className="avatar_img" src={this.state.picture} />
+      </div>)
+  }
 }
 
 export default App;
